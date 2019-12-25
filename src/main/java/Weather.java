c1064165b197ca3238c12214b2972fdc6f85031b
Rumeysa31 <Rumeysa31@users.noreply.github.com>
2019-12-25 12:22:14 +0300

    Initial commit

---
+++ src/main/java/Weather.java
@@ -0,0 +1,33 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class Weather {
+    public static void doHttpGet(){
+        //http://dataservice.accuweather.com/forecasts/v1/daily/15day/316649?apikey=316649
+        //http://dataservice.accuweather.com/forecasts/v1/daily/15day/<CITYID>?apikey=316649
+
+        String url = "http://dataservice.accuweather.com/forecasts/v1/daily/15day/316649?apikey=316649";
+
+        HttpClient client = HttpClients.createDefault();
+        HttpGet get = new HttpGet(url);
+        CloseableHttpResponse resp = null;
+
+        try {
+
+            resp = (CloseableHttpResponse) client.execute(get);
+            HttpEntity entity = resp.getEntity();
+            System.out.println("Json response");
+            System.out.println(EntityUtils.toString(entity));
+
+        }  catch (IOException ioe) { System.err.println("Something went wrong getting the weather: ");  ioe.printStackTrace(); }
+        catch (Exception e ){ System.err.println("Unknown error: "); e.printStackTrace(); }
+    }
+
+    }
+
